be39c0de4881e06b185abd619bda8a6c67d11fa8
Jon Haddad <jon@jonhaddad.com>
2013-10-17 17:30:45 -0700

    working on group creation

--- roadhouse/groups.py
+++ roadhouse/groups.py
@@ -7,14 +7,53 @@ def sync(yaml_file_path, ec2_conn = None):
     # convenience method.  you'll probably always want this for simplicity
     # if ec2_conn is none, we will try to connect using the default settings
 
-    tmp = open(yaml_file_path, 'r').read()
-    yaml_as_dict = yaml.load(tmp)
-    return sync_yaml(yaml_as_dict, ec2_conn)
+    sgc = SecurityGroupsConfig.load(yaml_file_path)
+    sgc.configure(ec2_conn)
+    sgc.apply_changes()
+    return sgc
 
-def sync_yaml(y, ec2_conn):
-    assert isinstance(y, dict)
-    assert isinstance(ec2_conn, EC2Connection)
-    # get current groups
-    groups = ec2_conn.get_all_security_groups()
 
-    # create new groups
+class SecurityGroupsConfig(object):
+
+    existing_groups = None
+
+    def __init__(self, config):
+        """
+        config is a dictionary of configuration options
+        """
+        self.config = config
+        self.new_group_count = 0
+        self.updated_group_count = 0
+
+    def configure(self, ec2_conn):
+        self.ec2 = ec2_conn
+        return self
+
+    @classmethod
+    def load(cls, yaml_path):
+        tmp = open(yaml_path, 'r').read()
+        yaml_as_dict = yaml.load(tmp)
+        return SecurityGroupsConfig(yaml_as_dict)
+
+    def apply(self):
+        """
+        returns a list of new security groups that will be added
+        """
+        if self.existing_groups is None:
+            self.existing_groups = self.ec2.get_all_security_groups()
+
+        existing_group_names = [x.name for x in self.existing_groups]
+
+        for x,y in self.config.items():
+            options = y.get('options', {})
+            desc = options.get('description', " ")
+
+            if x not in existing_group_names:
+                # create the group
+                group = self.ec2.create_security_group(x, desc)
+                # set up ports
+                self.new_group_count += 1
+            else:
+                # update desc if it's wrong
+                self.updated_group_count += 1
+
--- roadhouse/tests/test_groups.py
+++ roadhouse/tests/test_groups.py
@@ -1,27 +1,35 @@
 import unittest
+import boto
 from moto import mock_ec2
-
-
+import mock
+from roadhouse import groups
+import yaml
 
 # needs to include ports, TCP/UDP, and
-sample1 = """
 
-test_database_group: some useful description
-    - tcp port 22-40 192.160.1.1/32
-    - tcp port 80 192.168.1.1/32
-    - udp port 999 sg-whatever
-"""
+class BaseConfigTestCase(unittest.TestCase):
+    def setUp(self):
+        self.ec2 = boto.connect_ec2('somekey', 'somesecret')
 
-class SyncTest(unittest.TestCase):
-    def test_
+    @property
+    def config(self):
+        return groups.SecurityGroupsConfig.load('sample.yaml').configure(self.ec2)
 
-class CreationTest(unittest.TestCase):
-    # ensures new group is created
+
+class CreationTest(BaseConfigTestCase):
+    # ensures new groups are created
+
     @mock_ec2
-    def test_creation(self):
-        pass
+    def test_creation_no_existing_groups(self):
+        c = self.config
+        self.assertGreater(c.new_group_count, 0)
+        self.assertEqual(c.updated_group_count, 0)
 
-
-class RuleUpdateTest(unittest.TestCase):
-    pass
+    @mock_ec2
+    def test_no_description(self):
+        tmp = {"test_no_description":
+                   {"options": {} }}
+        config = groups.SecurityGroupsConfig(tmp).configure(self.ec2)
+        config.apply()
+        self.assertGreater(config.new_group_count, 0)
 
