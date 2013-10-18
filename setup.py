44cc07ee22a7b24dc270a63fc507879b2537592e
Jon Haddad <jon@jonhaddad.com>
2013-10-17 17:32:45 -0700

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='roadhouse',
+    version='0.1',
+    packages=['roadhouse'],
+    url='https://github.com/StartTheShift/roadhouse',
+    license='BSD 2 Clause',
+    author='Jon Haddad',
+    author_email='jon@shift.com',
+    description='AWS Configuration Mangement '
+)
