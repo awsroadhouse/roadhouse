ce64fcdefdba598ff5491ff04e875b42d4a45606
Jon Haddad <jon@jonhaddad.com>
2013-10-22 18:39:00 -0700

    updating setup

--- setup.py
+++ setup.py
@@ -4,9 +4,20 @@ setup(
     name='roadhouse',
     version='0.1',
     packages=['roadhouse'],
+    install_requires=["boto", "pyparsing", "PyYAML"],
+    classifiers=["Development Status :: 3 - Alpha",
+                 "Topic :: Security",
+                 "Intended Audience :: Information Technology",
+                 "Intended Audience :: System Administrators",
+                 "License :: OSI Approved :: BSD License",
+                 "Operating System :: POSIX :: Linux",
+                 "Operating System :: MacOS :: MacOS X"
+                 ],
+
     url='https://github.com/StartTheShift/roadhouse',
     license='BSD 2 Clause',
     author='Jon Haddad',
     author_email='jon@shift.com',
-    description='AWS Configuration Mangement '
+    description='AWS Configuration Mangement',
+    keywords="aws, yaml, configuration"
 )
