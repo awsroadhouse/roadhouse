d16de63317048419d47ea87ad0fb7e22d5df0fc7
Jon Haddad <jon@jonhaddad.com>
2014-04-21 16:53:46 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='roadhouse',
-    version='0.4',
+    version='0.5',
     packages=['roadhouse'],
     install_requires=["boto", "pyparsing", "PyYAML"],
     classifiers=["Development Status :: 4 - Beta",
