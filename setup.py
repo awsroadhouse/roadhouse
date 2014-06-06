d7c2c316fc20a04b8cae3357996c0ce4f51d44ea
Jon Haddad <jon@jonhaddad.com>
2014-06-06 12:31:50 -0700

    updated owner

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='roadhouse',
-    version='0.5',
+    version='0.6',
     packages=['roadhouse'],
     install_requires=["boto", "pyparsing", "PyYAML"],
     classifiers=["Development Status :: 4 - Beta",
@@ -14,10 +14,10 @@ setup(
                  "Operating System :: MacOS :: MacOS X"
                  ],
 
-    url='https://github.com/StartTheShift/roadhouse',
+    url='https://github.com/awsroadhouse/roadhouse',
     license='BSD 2 Clause',
     author='Jon Haddad',
-    author_email='jon@shift.com',
+    author_email='jon@jonhaddad.com',
     description='AWS Configuration Mangement',
     keywords="aws, yaml, configuration"
 )
